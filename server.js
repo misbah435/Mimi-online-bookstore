fad1e9bfd440e42d3d48b9e21c24a7a7aef0fa91
Misbah Shiakh <misbahshaikh.2434@gmail.com>
2026-09-14 12:04:10 +0530

    Configure EJS view engine and update root route
    Set EJS as the view engine and render the index page.

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.send("Welcome to Mimi Online Bookstore!");
+    res.render("index");
 });
 
 app.listen(PORT, () => {
